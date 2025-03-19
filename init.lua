01a04efd9e38f5c0013add43f406773a487b1019
Nguyễn Hoàng Anh Duy <duy.nguyen2@trustingsocial.com>
2025-03-19 17:21:09 +0700

    Extract global config and add more tab/indent settings

--- init.lua
+++ init.lua
@@ -1,13 +1,3 @@
+require("config.global")
 require("config.lazy")
-
-local set = vim.opt
-
-set.number = true
-set.relativenumber = true
-set.clipboard = "unnamedplus"
-
-set.tabstop 	= 4
-set.softtabstop = 4
-set.shiftwidth 	= 4
-set.expandtab 	= true
 
---
+++ lua/config/global.lua
@@ -0,0 +1,13 @@
+local set = vim.opt
+
+set.number = true
+set.relativenumber = true
+set.clipboard = "unnamedplus"
+
+set.tabstop 	= 4
+set.softtabstop = 4
+set.shiftwidth 	= 4
+set.expandtab 	= true
+set.shiftround  = true
+set.smartindent = true
+set.cindent     = true
