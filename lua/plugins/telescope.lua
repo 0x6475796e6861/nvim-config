31219b8b4c3aac87355d1263f1bac2dc5ae88fb4
Nguyễn Hoàng Anh Duy <duy.nguyen2@trustingsocial.com>
2025-03-19 11:35:48 +0700

    Configure plugin telescope to show hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,16 @@
 return {
-    {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          }
+        }
+      })
+    end
+  }
 }
