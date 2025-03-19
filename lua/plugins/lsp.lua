70cd5f62fe29182908b2b4eb8fea5d59bd066a7a
Nguyễn Hoàng Anh Duy <duy.nguyen2@trustingsocial.com>
2025-03-19 17:22:52 +0700

    Init LSP plugins and setup handlers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,19 +1,23 @@
 return {
   {
     "neovim/nvim-lspconfig", tag = "v1.7.0",
+    opt = {},
   },
   {
     "williamboman/mason.nvim", tag = "v1.11.0",
     dependencies = { "neovim/nvim-lspconfig" },
-    config = function()
-      require("mason").setup()
-    end
+    opts = {}
   },
   {
     "williamboman/mason-lspconfig.nvim", tag = "v1.32.0",
     dependencies = { "williamboman/mason.nvim" },
     config = function()
       require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers({
+        function (server_name)
+          require("lspconfig")[server_name].setup({})
+        end
+      })
     end
   }
 }
