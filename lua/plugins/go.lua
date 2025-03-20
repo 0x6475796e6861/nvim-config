904bbe26fe79e84d7e164e692c188d304f04d144
Nguyễn Hoàng Anh Duy <duy.nguyen2@trustingsocial.com>
2025-03-20 16:31:59 +0700

    Clean up plugin config go.nvim

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,16 +1,12 @@
-return {
-  {
-    "ray-x/go.nvim",
-    dependencies = {  -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = {"CmdlineEnter"},
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+return {{
+  "ray-x/go.nvim", tag = "v0.10.0",
+  dependencies = {
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
   },
-}
+  opt = {},
+  event = {"CmdlineEnter"},
+  ft = { "go", "gomod" },
+  build = ':lua require("go.install").update_all_sync()'
+}}
