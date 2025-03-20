6ca8cb99d011150b275f1b33fef588f4c233837e
Nguyễn Hoàng Anh Duy <duy.nguyen2@trustingsocial.com>
2025-03-20 17:15:52 +0700

    Disable telescope preview for large files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,9 @@ return {{
   "nvim-telescope/telescope.nvim", tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
+    defaults = {
+      preview = { filesize_limit = 10 },
+    },
     pickers = {
       find_files = { hidden = true }
     }
