135986fa9f98ecb605147a994484491e7add4a7e
Nguyễn Hoàng Anh Duy <duy.nguyen2@trustingsocial.com>
2025-03-20 16:33:27 +0700

    Clean up plugin config telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,9 @@
-return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        pickers = {
-          find_files = {
-            hidden = true,
-          }
-        }
-      })
-    end
+return {{
+  "nvim-telescope/telescope.nvim", tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    pickers = {
+      find_files = { hidden = true }
+    }
   }
-}
+}}
