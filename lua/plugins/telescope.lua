2e287b7a15667162b5afe05fd4c78fe552aa41fc
Nguyễn Hoàng Anh Duy <duy.nguyen2@trustingsocial.com>
2025-03-24 21:04:56 +0700

    Add shortcuts to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,12 @@ return {{
   opts = {
     defaults = {
       preview = { filesize_limit = 10 },
+      mappings = {
+        i = {
+          ["<C-k>"] = "move_selection_next",
+          ["<C-j>"] = "move_selection_previous",
+        }
+      }
     },
     pickers = {
       find_files = { hidden = true }
